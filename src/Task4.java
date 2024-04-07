a6381b8f69ccdff8766b9211d2d0877cfe64c731
kanazholdasov <156598163+kanazholdasov@users.noreply.github.com>
2024-04-07 21:02:24 +0500

    Task4

--- src/Task4.java
+++ src/Task4.java
@@ -1,3 +1,12 @@
+/**
+ * This method calculates the factorial of a number using recursion.
+ * The base case is when n is 0, in which case 1 is returned.
+ * Otherwise, the method calls itself with n-1 and multiplies the result by n.
+ * Time complexity: O(n), where n is the input number.
+ * @param n The number for which factorial needs to be calculated.
+ * @return The factorial of the input number.
+ */
+
 import java.util.Scanner;
 
 public class Task4 {
